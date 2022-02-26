a220567495725e04608b64cf049a7a261328b834
Techy0302 <99090768+Techy0302@users.noreply.github.com>
2022-02-26 23:35:52 +0530

    Update potato_j7elte.mk

--- potato_j7elte.mk
+++ potato_j7elte.mk
@@ -20,8 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit common Lineage phone.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from Potato vendor
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
-WITH_GMS_MINIMAL := true
+
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := potato_j7elte
