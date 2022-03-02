f4ba2ab172d2d27b942eee221d537ea23c991012
Techy0302 <99090768+Techy0302@users.noreply.github.com>
2022-03-02 17:10:29 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_j7elte.mk
+    $(LOCAL_PATH)/cesium_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j7elte-user \
-    lineage_j7elte-userdebug \
-    lineage_j7elte-eng
+    cesium_j7elte-user \
+    cesium_j7elte-userdebug \
+    cesium_j7elte-eng
