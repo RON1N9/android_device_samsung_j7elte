d7be31b022dd9bbe7408ec8352b584596175931a
Techy0302 <99090768+Techy0302@users.noreply.github.com>
2022-03-06 17:11:37 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/aosp_j7elte.mk
+    $(LOCAL_PATH)/pixys_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_j7elte-user \
-    aosp_j7elte-userdebug \
-    aosp_j7elte-eng
+    pixys_j7elte-user \
+    pixys_j7elte-userdebug \
+    pixys_j7elte-eng
