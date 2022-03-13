9333c942898e47fd25a65e1ac3620da05140b14a
Techy0302 <99090768+Techy0302@users.noreply.github.com>
2022-03-13 11:56:20 +0530

    Update and rename p404_j7elte.mk to aosp_j7elte.mk

--- aosp_j7elte.mk
+++ aosp_j7elte.mk
@@ -20,14 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit common Lineage phone.
-$(call inherit-product, vendor/404/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 720
 
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := p404_j7elte
+PRODUCT_NAME := aosp_j7elte
 PRODUCT_DEVICE := j7elte
 PRODUCT_MODEL := SM-J700F
 PRODUCT_BRAND := samsung
